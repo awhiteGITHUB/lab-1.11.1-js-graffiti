bdcb4dd8333dd6217e0cad8bd9493b00bc78e851
unknown <anthonywhite967@gmail.com>
2021-10-20 09:28:18 -0400

    finished my graffiti project

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ for(let i = 0; i < dogImages.length; i++){
 }
 
 let dogNames = document.querySelectorAll('.dog-name')
-for(let i = 0; i < dogNames.textAlign; i++){
-    dogNames[i].style.textAlign = "left"
+for(let i = 0; i < dogNames.length; i++){
+   dogNames[i].style.textAlign = "left"
+
+}
+
+let dogRotate = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogRotate.length; i++){
+dogRotate[i].style.transform = "rotate(180deg)"
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'blue'
+
+let footer1 = document.querySelector('.footer')
+footer1.style.borderStyle = 'solid'
+
+let footer2 = document.querySelector('.footer')
+footer2.style.textAlign = "center"
+
+
 // Select some elements...
